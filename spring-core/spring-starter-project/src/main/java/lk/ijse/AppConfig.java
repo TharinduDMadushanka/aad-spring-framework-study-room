3022cbf3116c757823800b738ad66e83c0966313
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-07 22:31:14 +0530

    scan components using @componentscan in app config

--- spring-core/spring-starter-project/src/main/java/lk/ijse/AppConfig.java
+++ spring-core/spring-starter-project/src/main/java/lk/ijse/AppConfig.java
@@ -1,12 +1,14 @@
 package lk.ijse;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class AppConfig {
     public AppConfig(){
-//        System.out.println("AppConfig Instantiated");
+        System.out.println("AppConfig Instantiated");
 
     }
 }
