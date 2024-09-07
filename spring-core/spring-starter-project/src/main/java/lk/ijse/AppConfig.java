5983b83ced0c6733f6189bbbfca9939eba53763c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-07 22:14:28 +0530

    add config class

--- spring-core/spring-starter-project/src/main/java/lk/ijse/AppConfig.java
+++ spring-core/spring-starter-project/src/main/java/lk/ijse/AppConfig.java
@@ -1,5 +1,8 @@
 package lk.ijse;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
     public AppConfig(){
         System.out.println("AppConfig Instantiated");
