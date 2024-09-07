7a26afbb2e48138d2309bc4556bd7fa1a64d5863
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-07 22:17:04 +0530

    create the application context

--- spring-core/spring-starter-project/src/main/java/lk/ijse/AppConfig.java
+++ spring-core/spring-starter-project/src/main/java/lk/ijse/AppConfig.java
@@ -1,10 +1,16 @@
 package lk.ijse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
     public AppConfig(){
         System.out.println("AppConfig Instantiated");
+
+        //create the application context
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
     }
 }
