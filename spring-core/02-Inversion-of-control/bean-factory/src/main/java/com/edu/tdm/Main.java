0358a5d32a168244e28d03180be2f58ce6611657
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-10 19:16:37 +0530

    add spring-beans dependency for get XmlBeanFactory

--- spring-core/02-Inversion-of-control/bean-factory/src/main/java/com/edu/tdm/Main.java
+++ spring-core/02-Inversion-of-control/bean-factory/src/main/java/com/edu/tdm/Main.java
@@ -1,7 +1,13 @@
 package com.edu.tdm;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+
     }
 }
