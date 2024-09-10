7e5cc7b1aa0471fec228eee667ee3a4bb4b2deb1
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-10 19:19:34 +0530

    crated bean.xml

--- spring-core/02-Inversion-of-control/bean-factory/src/main/java/com/edu/tdm/Main.java
+++ spring-core/02-Inversion-of-control/bean-factory/src/main/java/com/edu/tdm/Main.java
@@ -7,7 +7,14 @@ import org.springframework.core.io.ClassPathResource;
 public class Main {
     public static void main(String[] args) {
 
+        // Load the XML configuration file
         BeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+
+        // Retrieve the Client bean from the BeanFactory
+        Client client = (Client) factory.getBean("client");
+
+        // Perform the task, which will invoke the injected Service
+        client.performTask();
 
     }
 }
