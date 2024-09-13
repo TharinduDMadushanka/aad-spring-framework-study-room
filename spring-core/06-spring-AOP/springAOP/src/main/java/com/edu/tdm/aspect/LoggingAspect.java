6a62af47f8a567fcf83a10461e911d06054118ca
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-13 18:35:08 +0530

    completed LoggingAspect class in aspect pkg

--- spring-core/06-spring-AOP/springAOP/src/main/java/com/edu/tdm/aspect/LoggingAspect.java
+++ spring-core/06-spring-AOP/springAOP/src/main/java/com/edu/tdm/aspect/LoggingAspect.java
@@ -1,7 +1,28 @@
 package com.edu.tdm.aspect;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class LoggingAspect {
+
+    @Before("execution(* com.edu.tdm.service.PaymentService.processPayment(..))")
+    public void logBefore(){
+        System.out.println("LoggingAspect: Payment processing started...");
+    }
+
+    @After("execution(* com.edu.tdm.service.PaymentService.processPayment(..))")
+    public void logAfter(){
+        System.out.println("LoggingAspect: Payment processing completed.");
+    }
+
+    @AfterReturning("execution(* com.edu.tdm.service.PaymentService.processPayment(..))")
+    public void logAfterReturning(){
+        System.out.println("LoggingAspect: Payment processed successfully.");
+    }
+
 }
