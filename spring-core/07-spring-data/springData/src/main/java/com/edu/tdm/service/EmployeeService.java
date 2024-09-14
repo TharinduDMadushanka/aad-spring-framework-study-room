0e18254e7fecbfea7bd37b015111bb7392dc1993
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-14 20:15:36 +0530

    completed EmployeeService class

---
+++ spring-core/07-spring-data/springData/src/main/java/com/edu/tdm/service/EmployeeService.java
@@ -0,0 +1,37 @@
+package com.edu.tdm.service;
+
+import com.edu.tdm.entity.Employee;
+import com.edu.tdm.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public void deleteEmployeeById(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+}
+
+/**
+ * @Service: Marks the class as a service in the Spring context.
+ * @Autowired: Injects the repository dependency.
+ */
