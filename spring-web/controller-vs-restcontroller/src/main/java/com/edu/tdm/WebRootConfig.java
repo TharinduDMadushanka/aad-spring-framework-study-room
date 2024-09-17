a894b65356a9172cb63c3454c471bca6d2cf59d7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-17 20:12:49 +0530

    created WebRootConfig class

---
+++ spring-web/controller-vs-restcontroller/src/main/java/com/edu/tdm/WebRootConfig.java
@@ -0,0 +1,10 @@
+package com.edu.tdm;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebRootConfig {
+    public WebRootConfig() {
+        System.out.println("WebRootConfig Instantiated -constructor()");
+    }
+}
