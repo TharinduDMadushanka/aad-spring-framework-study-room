2b30626f15e74311663097121281a307821cdecd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-17 20:27:07 +0530

    created addResourceHandlers & viewResolver methods in WebAppConfig class

--- spring-web/controller-vs-restcontroller/src/main/java/com/edu/tdm/WebAppConfig.java
+++ spring-web/controller-vs-restcontroller/src/main/java/com/edu/tdm/WebAppConfig.java
@@ -1,15 +1,30 @@
 package com.edu.tdm;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
 
     public WebAppConfig() {
         System.out.println("WebAppConfig Instantiated -constructor()");
     }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/WEB-INF/pages/**").addResourceLocations("/WEB-INF/WEB-INF/pages/**");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        return new InternalResourceViewResolver("/WEB-INF/WEB-INF/pages/",".html");
+    }
+
 
 
 }
