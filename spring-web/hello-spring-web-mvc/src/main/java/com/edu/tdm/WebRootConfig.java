c403c0fcbfc5bc59d2d392039c2c8a0c15894556
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-18 08:30:36 +0530

    complete web-mvc-structure

--- spring-web/hello-spring-web-mvc/src/main/java/com/edu/tdm/WebRootConfig.java
+++ spring-web/hello-spring-web-mvc/src/main/java/com/edu/tdm/WebRootConfig.java
@@ -5,3 +5,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebRootConfig {
 }
+
+/**
+ * Purpose: This class is a placeholder for the root application context configuration.
+ * Typically, you use this class to configure beans that are shared across your entire application (like services, repositories, etc.).
+ * Empty for Now: Since it’s currently empty, it’s not doing much, but later on, you might configure data sources,
+ * transaction management, or security here.
+ */
