e7d276a4accc5b1b39947be04b599458349af62c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-18 09:22:20 +0530

    created WebRootConfig class

---
+++ spring-web/simple-web-crud-example/src/main/java/com/edu/tdm/WebRootConfig.java
@@ -0,0 +1,9 @@
+package com.edu.tdm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:hibernate.properties")
+public class WebRootConfig {
+}
